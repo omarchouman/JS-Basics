667b8553892274fa8d1479e33e409190f5ddca5e
Omar Chouman <omar.chouman0@gmail.com>
2021-05-27 16:22:57 +0300

    Step_6 Done

---
+++ javascript-basics-02/step 6/Step_6.js
@@ -0,0 +1,15 @@
+let any = document.querySelector("#image1")
+any.addEventListener("mouseover", function() {
+    document.querySelector("#image1").src = "images/image1_2.jpg";
+    document.querySelector("#image2").src = "images/image2_2.jpg";
+    document.querySelector("#image3").src = "images/image3_2.jpg";
+    document.querySelector("#image4").src = "images/image4_2.jpg";
+    document.querySelector("#image5").src = "images/image5_2.jpg";
+});
+any.addEventListener("mouseout", function() {
+    document.querySelector("#image1").src = "images/image1.jpg";
+    document.querySelector("#image2").src = "images/image2.jpg";
+    document.querySelector("#image3").src = "images/image3.jpg";
+    document.querySelector("#image4").src = "images/image4.jpg";
+    document.querySelector("#image5").src = "images/image5.jpg";
+});
