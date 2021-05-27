122c6e59547e1665a14df5ecb5c1989e4d3153e7
Omar Chouman <omar.chouman0@gmail.com>
2021-05-27 14:21:14 +0300

    Step_4 Done

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name = prompt("Enter Your Name:");
+var surname = prompt("Enter Your Surname:");
+var city = prompt("Enter Your City:");
+
+alert("Name : " + name + "\n" + "Surname : " + surname + "\n" + "City : " + city);
