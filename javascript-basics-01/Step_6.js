544c62719546257970f9f7afd48e8a191978e63d
Omar Chouman <omar.chouman0@gmail.com>
2021-05-27 14:50:33 +0300

    Step_6 Done

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,7 @@
+function run() {
+    var n1 = document.getElementById("first_number").value;
+    var n2 = document.getElementById("second_number").value;
+    var remainder = n1 % n2;
+    
+    alert("The Remainder of Both Numbers is : " + remainder);
+}
