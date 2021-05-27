a1912981384caa534a8d78c02ef1324caf976418
Omar Chouman <omar.chouman0@gmail.com>
2021-05-27 15:16:51 +0300

    Step_8 Done

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,10 @@
+var age = document.getElementById("age");
+
+function run() {
+    if(age.value > 18) {
+        alert("you are over 18");
+    } else {
+        alert("you are under 18");
+    }
+}
+ 
